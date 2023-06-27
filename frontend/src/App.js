ede955100ad51e7085d9dce0317baf5d77ca1443
Ruben Lozano <yaricklozanomoctezuma1@gmail.com>
2023-06-27 15:43:58 -0600

    Amazona testing

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">AMAZONA</a>
+      </header>
+      <main>List Porducts</main>
+    </div>
+  );
+}
+
+export default App;
